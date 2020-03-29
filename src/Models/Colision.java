a77d77ba442acd8527a9017a808dc98a9be424bc
Miguel <mdiluca@itba.edu.ar>
2020-03-29 18:15:50 -0300

    initial

---
+++ src/Models/Colision.java
@@ -0,0 +1,12 @@
+package Models;
+
+public class Colision implements Comparable {
+
+    Particle particle1, particle2;
+    float timeLeft;
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
